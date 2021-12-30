650b50af49d9f0e8ba328e80896114fb248b949a
mohd abbas <mohammed.abbas0332@gmail.com>
2021-12-30 17:15:33 +0530

    Update Purpose line

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ This is deployement script used while deploying  the servers.
 jenkins insallation
 #!/bin/bash ----> dev branch
 #Author : Sohail
-#Sript is as follows
-
+#purpose : Learning
+#Script is as follows
