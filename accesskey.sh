295fddfdf4dc14b86eaa86dc60e47691f17d0a65
mohd abbas <mohammed.abbas0332@gmail.com>
2021-12-31 17:42:18 +0530

    Perfect access token script

--- accesskey.sh
+++ accesskey.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 echo "enter your github personal token access"
 read token
-
 cat ~/.ssh/id_rsa.pub
-
-if [ $? eq 0 ]
+if [ $? -eq 0 ]
 then
-echo "SSH keys are already present"
+	echo "SSH keys are already present"
 else
-echo "SSH keys are not present..., Create the SSH keys using key-gen command"
-ssh-keygen -t rsa
-echo "SSH keys are succesfully generated"
+	echo "SSH keys are not present..., Create the SSH keys using key-gen command"
+	ssh-keygen -t rsa
+	echo "SSH keys are succesfully generated"
 fi
 
-sshkey=`cat ~/.ssh/id_rsa.pub
+sshkey=`cat ~/.ssh/id_rsa.pub`
 
 if [ $? -eq 0 ]
 then
 echo "Copying key to the github account"
-curl -X POST -H "content-type: application/json" -d "{\"title\": \"SSHKEY\",\"key\": \"$sshkey\"}" "https://api.github.com/user/key?/access_token=$token"
+curl -X POST -H "Content-type: application/json" -d "{\"title\": \"SSHKEY\",\"key\": \"$sshkey\"}" "https://api.github.com/user/keys?/access_token=$token"
 if [ $? -eq 0 ]
 then 
 echo "Successfully copied the token to github"
